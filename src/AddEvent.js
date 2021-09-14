50ac6c161fdf6c066fbbbb95db256aaa1de02692
jaenwawe <nwawe.jae@gmail.com>
2021-09-14 11:18:06 -0500

    events

--- src/AddEvent.js
+++ src/AddEvent.js
@@ -3,6 +3,24 @@ function AddEvent() {
   return (
     <div>
     {console.log("AddEvent!")}
+    { {"id": 1,
+      "name": "Career Coach AMA",
+      "description": "Ask a Career Coach anything about the job search process!",
+      "category": "career",
+      "date": "Thursday, September 16th" ,
+      "time": "11:30am ET/8:30am PT",
+      "location": "online",
+      "zoom": "https://flatironschool.zoom.us/j/98316664785?pwd=WDMxS3psMnpzVVhFMy9sVEFsRDZJQT09" ,
+      "image": "https://upload.wikimedia.org/wikipedia/commons/3/36/Careers_blackboard.jpg",
+      "attending": ["Melissa", "Gabriel", "Jae"]}}
+
+      
+
+
+
+
+
+
     </div>
   )
 }
